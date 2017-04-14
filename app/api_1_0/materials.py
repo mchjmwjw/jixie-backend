e03bf1037ce25e4763bc860abc73ac638ce910ec
mchjmwjw <mchjmwjw@163.com>
2017-04-14 18:00:50 +0800

    交互ing

--- app/api_1_0/materials.py
+++ app/api_1_0/materials.py
@@ -1,11 +1,35 @@
 # coding=utf8
 from . import api
-from flask import request, json, make_response
+from flask import request, json, make_response, jsonify
+from ..models import Material, db
+
+
+def serialize_instance(obj):
+    d = {'__classname__': type(obj).__name__}
+    d.update(vars(obj))
+    return d
 
 
 @api.route('/materials/save/', methods=['POST'])
 def save():
-    i = request.json
-    rst = make_response('xxx')
+    data = request.get_json()
+    material = Material(data)
+    # for(k, v) in data.items():
+    db.session.add(material)
+    db.session.commit()
+    # 返回所有数据
+    materials = Material.query.all()
+    data2 = json.dumps(materials, default=serialize_instance)
+    rst = make_response(data2)
     rst.headers['Access-Control-Allow-Origin'] = '*'
     return rst
+
+
+@api.route('/materials/get/', methods=['GET'])
+def get():
+    materials = Material.query.all()
+    data2 = json.dumps(materials[0].to_json())  # , default=serialize_instance)
+    rst = make_response(data2)
+    rst.headers['Access-Control-Allow-Origin'] = '*'
+    return rst
+
---
+++ app/models/material.py
@@ -0,0 +1,36 @@
+from .. import db
+
+
+class Material(db.Model):
+    __tablename__ = 'materials'
+    phid = db.Column(db.Integer, primary_key=True)
+    m_no = db.Column(db.String(255), unique=True)
+    m_name = db.Column(db.String(1000))
+    m_unit = db.Column(db.String(50))
+    m_amount = db.Column(db.Integer)
+    m_kind = db.Column(db.String(255))
+    m_remark = db.Column(db.String(1000))
+
+    def __init__(self, obj):
+        mobj = obj['mdata']
+        self.m_amount = mobj['m_amount']
+        self.phid = mobj['phid']
+        self.m_no = mobj['m_no']
+        self.m_name = mobj['m_name']
+        self.m_unit = mobj['m_unit']
+        self.m_kind = mobj['m_kind']
+        self.m_remark = mobj['m_remark']
+
+    def to_json(self):
+        mjson = {
+            'phid': self.phid,
+            'm_amount': self.m_amount,
+            'm_no': self.m_no,
+            'm_name': self.m_name,
+            'm_unit': self.m_unit,
+            'm_kind': self.m_kind,
+            'm_remark': self.m_remark
+        }
+
+    def __repr__(self):
+        return '<Material %r>' % self.m_name
