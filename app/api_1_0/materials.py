69b83481d3aae7bc2fe32e1c964ed2a004534483
mchjmwjw <mchjmwjw@163.com>
2017-04-25 17:51:08 +0800

    树页面完成

--- app/api_1_0/materials.py
+++ app/api_1_0/materials.py
@@ -32,18 +32,42 @@ def get():
     rst.headers['Access-Control-Allow-Origin'] = '*'
     return rst
 
-
+# 保存tree数据
 @api.route('/kindtree/save/', methods=['POST'])
-@cross_origin()
+# @cross_origin()
 def savekind():
     data = request.get_json()
+    dic = {}
+    datalist = []
+    # 保存新增的节点
     for v in data['mdata']:
         kind = Kind(v)
-        db.session.add(kind)
-        db.session.commit()
-    return jsonify(res='success')
+        if kind.phid < 0:
+            oldid = kind.phid
+            kind.phid = None
+            db.session.add(kind)
+            db.session.commit()
+            dic[oldid] = kind.phid
+        datalist.append(kind)
+    # 保存修改/未修改的节点
+    for d in datalist:
+        if (d.pid is not None) and d.pid < 0:
+            d.pid = dic[d.pid]
+        kindDic = {}
+        for (k, v) in d.__dict__.items():
+            if(k is not '_sa_instance_state'):
+                kindDic[k] = v
+        k = Kind.query.filter_by(phid=d.phid).update(kindDic)
+    # 删除已remove的节点
+    for index in data['removeIds']:
+        kind = Kind.query.filter_by(phid=index).first()
+        db.session.delete(kind)
 
+    db.session.commit()
+    return gettree()
+    # return jsonify(res='success')
 
+# 获取树
 @api.route('/kindtree/get/', methods=['GET'])
 def gettree():
     kinds = Kind.query.all()
